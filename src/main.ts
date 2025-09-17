e6129dec360d26a8521c92f2ddc6e013a07434e7
Konstantin Novikov <nov.konstantin@gmail.com>
2025-09-18 01:29:27 +0200

    asd

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ import { initFederation } from '@angular-architects/native-federation';
 
 initFederation('/federation.manifest.json')
   .catch(err => console.error(err))
-  .then(() => import('./bootstrap.js'))
+  .then(() => import('./bootstrap'))
   .catch(err => console.error(err));
