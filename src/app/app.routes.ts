8a94af44b24f6c606cecef4868ae4304860b8a02
Konstantin Novikov <nov.konstantin@gmail.com>
2025-09-18 00:48:54 +0200

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,16 @@ import { loadRemoteModule } from '@angular-architects/native-federation';
 
 export const appRoutes: Route[] = [
     {
+        path: '',
+        redirectTo: 'user',
+        pathMatch: 'full'
+    },
+    {
         path: 'user',
         loadComponent: () => import('./user/user.page').then(m => m.UserPage)
     },
     {
         path: 'settings',
         loadComponent: () => import('./settings/settings.page').then(m => m.SettingsPage)
-    },
-    {
-        path: 'myapp',
-        loadComponent: () => loadRemoteModule({
-            remoteEntry: 'http://localhost:4200/remoteEntry.json',
-            exposedModule: './AppComponent'
-        }).then(m => m.AppComponent)
     }
 ];
