814061a53fd4744b4579e286945ff8c62e44bc07
Konstantin Novikov <nov.konstantin@gmail.com>
2025-09-18 08:39:02 +0200

    fix

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,18 @@
-import { Component, inject, OnInit } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { AsyncPipe, CommonModule } from "@angular/common";
 import { Store } from "@ngrx/store";
 import { selectLanguage, selectNotifications, selectTheme } from "./home.selectors";
 
 @Component({
     standalone: true,
+    imports: [CommonModule, AsyncPipe],
     selector: 'app-home',
     templateUrl: './home.page.html',
     styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage {
     #store = inject(Store)
     theme$ = this.#store.select(selectTheme);
     language$ = this.#store.select(selectLanguage)
     notifications$ = this.#store.select(selectNotifications);
-
-    ngOnInit(): void {
-        console.log('HomePage initialized');
-    }
 }
--- src/app/user/user.page.ts
+++ src/app/user/user.page.ts
@@ -1,13 +1,24 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { AsyncPipe, CommonModule } from "@angular/common";
+import { Store } from "@ngrx/store";
+import {
+    selectCurrentUser,
+    selectIsAuthenticated,
+    selectUserLoading,
+    selectUserError
+} from "./user.selectors";
 
 @Component({
     standalone: true,
+    imports: [CommonModule, AsyncPipe],
     selector: 'app-user',
     templateUrl: './user.page.html',
     styleUrls: ['./user.page.scss'],
 })
-export class UserPage implements OnInit {
-    ngOnInit(): void {
-        console.log('UserPage initialized');
-    }
+export class UserPage {
+    #store = inject(Store);
+    currentUser$ = this.#store.select(selectCurrentUser);
+    isAuthenticated$ = this.#store.select(selectIsAuthenticated);
+    loading$ = this.#store.select(selectUserLoading);
+    error$ = this.#store.select(selectUserError);
 }
